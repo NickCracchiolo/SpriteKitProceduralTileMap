52d7759f13b59385faf1e1fb9e26d975892ac579
Nick <nick@cracchiolo.com>
2018-05-08 18:08:01 -0700

    Added License and TileMap code

--- ProceduralTileMap/GameScene.swift
+++ ProceduralTileMap/GameScene.swift
@@ -2,88 +2,117 @@
 //  GameScene.swift
 //  ProceduralTileMap
 //
-//  Created by Nicholas Cracchiolo on 5/8/18.
-//  Copyright © 2018 Nick Cracchiolo. All rights reserved.
+//  MIT License
 //
+//  Copyright (c) 2018 Nick Cracchiolo
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+	
+	override func didMove(to view: SKView) {
+		let map = createMap()
+		map.anchorPoint = CGPoint(x:0.5, y:0.5)
+		self.addChild(map)
+	}
+	
+	func touchDown(atPoint pos : CGPoint) {
+		
+	}
+	
+	func touchMoved(toPoint pos : CGPoint) {
+		
+	}
+	
+	func touchUp(atPoint pos : CGPoint) {
+		
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+	}
+	
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+	}
+	
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+	}
+	
+	override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+	}
+	
+	override func update(_ currentTime: TimeInterval) {
+		// Called before each frame is rendered
+	}
+	
+	func createNoiseMap() -> GKNoiseMap {
+		//Get our noise source, this can be customized further
+		let source = GKPerlinNoiseSource()
+		//Initalize our GKNoise object with our source
+		let noise = GKNoise.init(source)
+		//Create our map
+		let map = GKNoiseMap.init(noise, size: vector2(1.0, 1.0), origin: vector2(0, 0), sampleCount: vector2(100, 100), seamless: true)
+		return map
+	}
+	
+	func createMap() -> SKTileMapNode {
+		//Get out noise map we made above
+		let noiseMap = createNoiseMap()
+		//Define our SKTileSet:
+		//This is the name of the Set within the .sks TileSet file
+		let tileSet = SKTileSet(named: "Sample Isometric Tile Set")!
+		//Define our Tile Size:
+		//This depends on the set but the Isometric set uses 128x64
+		let tileSize = CGSize(width: 128, height: 64)
+		//Define our Columns and Rows
+		let rows = 50, cols = 50
+		//Create our Tile Map
+		let map = SKTileMapNode(tileSet: tileSet, columns: cols, rows: rows, tileSize: tileSize)
+		map.enableAutomapping = true
+		for col in 0..<cols {
+			for row in 0..<rows {
+				//Get a value from our Noise Map returns -1.0 to 1.0
+				let val = noiseMap.value(at: vector2(Int32(row), Int32(col)))
+				//We will then decide what tiles correspond to what value
+				switch val {
+				//Set as our water tile
+				case -1.0..<(-0.5):
+					if let g = tileSet.tileGroups.first(where: {
+						($0.name ?? "") == "Water"}) {
+						map.setTileGroup(g, forColumn: col, row: row)
+					}
+					//ADD OTHER CASES HERE FOR OTHER TILE GROUPS
+				//Set as our grass tile
+				default:
+					if let g = tileSet.tileGroups.first(where: {
+						($0.name ?? "") == "Grass"}) {
+						map.setTileGroup(g, forColumn: col, row: row)
+					}
+				}
+			}
+		}
+		return map
+	}
 }
